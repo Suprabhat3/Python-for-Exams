a4e706a78747d5b42ad23577ef7b3fbf82a631ee
Suprabhat <sdk26150@gmail.com>
2024-11-25 19:42:26 +0530

    Update Check prime numbers.py

--- Python for Seasonal/Check prime numbers.py
+++ Python for Seasonal/Check prime numbers.py
@@ -1,13 +1,13 @@
 n = int(input("Enter a number :"))
 var = 0
 if n <= 0:
-    print("your number is not a prime number")
+    print("your number is not a Prime number")
     exit()
 for i in range(2,n):
     if n%i == 0:
         var += 1
         break
 if var == 1:
-    print("your number is not a prime number")    
+    print("your number is not a Prime number")    
 else:
-    print("your number is a prime number")
+    print("your number is a Prime number")
